74db39c5ac25c537233ea73061876d4f2391b066
maaz-hgl <maaz.hagalwadi570@gmail.com>
2025-12-06 22:35:25 +0530

    commit

--- backend/src/main/java/com/tripgo/backend/model/entities/Booking.java
+++ backend/src/main/java/com/tripgo/backend/model/entities/Booking.java
@@ -24,7 +24,7 @@ public class Booking {
     private UUID id;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id")  // SET NULL on delete
+    @JoinColumn(name = "user_id")
     private User user;
 
     @ManyToOne(fetch = FetchType.LAZY)
