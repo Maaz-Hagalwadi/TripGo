c086f8bcf773c97684c1e78791649cfff2269e4e
Maaz-Hagalwadi <114414940+Maaz-Hagalwadi@users.noreply.github.com>
2026-03-30 22:19:44 +0530

    fix4

--- frontend/TripGo-frontend/src/features/auth/pages/VerifyEmail.jsx
+++ frontend/TripGo-frontend/src/features/auth/pages/VerifyEmail.jsx
@@ -4,7 +4,7 @@ import { Box, Typography, Button, CircularProgress, Container } from '@mui/mater
 import { CheckCircle, Error } from '@mui/icons-material';
 import TripGoIcon from '../../../assets/icons/TripGoIcon';
 
-import { API_BASE_URL } from '../../config/env';
+import { API_BASE_URL } from '../../../config/env';
 
 const VerifyEmail = () => {
   const [searchParams] = useSearchParams();
